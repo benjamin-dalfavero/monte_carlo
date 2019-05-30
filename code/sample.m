14a2540d474594c9fc739e2c77b52b465a1a6f3f
Benjamin DalFavero <bdalfavero@scu.edu>
2019-05-30 13:13:28 -0700

    reflect changes from black_body?

--- code/sample.m
+++ code/sample.m
@@ -1,19 +1,18 @@
-function [y, xi] = sample(func, ny, nxi)
-% [y, xi] = sample(func, ny, nxi)
+function [y, xi] = sample(F, ny, nxi, g)
+% [y, xi] = sample(F, ny, nxi, g)
 % sample a cdf
 % y - array of sampled points
 % xi - random points solved for
-% func - function handle of CDF
+% F - function handle of CDF
 % ny - number of points for inerpolation table
 % nxi - number of random points to sample
+% g - initial guess for root finder
 
 % generate inerpolation table
 yint = zeros(1, ny); % accumulator for interpolation points
-f = @(x, n) func(x) - n/ny; % function to find roots of for all n
-g = 0.1; % initial guess
-tol = 1e-7; % tolerance
+tol = 1e-4; % tolerance
 for nn = 0:ny
-	yint(nn+1) = newton(@(x) f(x, nn), g, tol);
+	yint(nn+1) = newton(@(x) F(x) - (nn/ny), g, tol);
 	g = yint(nn+1); % reset guess
 end
 % sample random points by interpolating inverse
