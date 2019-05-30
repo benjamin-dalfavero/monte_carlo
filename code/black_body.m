d6e87016d38de826db6e10b7850a7c5a71f26103
Benjamin DalFavero <bdalfavero@scu.edu>
2019-05-30 13:12:59 -0700

    fix root finder

--- code/black_body.m
+++ code/black_body.m
@@ -7,12 +7,22 @@ dat = csvread('../data/black_body.csv');
 lt = dat(:, 1); % lambda * T
 R = dat(:, 2); % associated CDF value
 
-%% function to sample
-% we will sample the cdf by interpolating from the given table
-F = @(x) interp1(lt, R, x);
+%% numerically invert cdf
+% cdf function from interpolation table
+F = @(x) interp1(lt, R, x, 'pchip', 1000);
+% find interpolation points for inversion
+yint = zeros(1, np); % accumulator
+g = 7000; % initial guess
+for n = 0:np
+	yint(n+1) = newton(@(x) F(x) - (n/np), g, 1e-3);
+	g = yint(n+1); % reset guess
+end
+% sample random points
+xi = rand(1, N);
+n = floor(np * xi);
+u = np * xi - n;
+n = n+1; % account for one based indexing in vector operations
+y = yint(n) + (yint(n+1) - yint(n)).*u;
 
-%% sample cdf
-[y, xi] = sample(F, np, N);
-
-%% plot results
-plot(xi, y)
+root = newton(@(x) F(x) - 1.5, 300, 1e-4)
+F(root)
