bb8174ebff34f5a59d1a7ce876949b2d3cfa7fac
benjamin d dalfavero <bdalfavero@scu.edu>
2019-06-06 08:21:35 -0700

    fix calculation for n

--- code/black_body.m
+++ code/black_body.m
@@ -1,28 +1,16 @@
 %% black body radiation
 
 %% set up constants
-np = 100; % number of points in interpolation table
-N = 200; % number of bundles
+N = 100; % number of points in interpolation table
+nbundles = 200; % number of bundles
 dat = csvread('../data/black_body.csv');
 lt = dat(:, 1); % lambda * T
 R = dat(:, 2); % associated CDF value
 
 %% numerically invert cdf
-% cdf function from interpolation table
-F = @(x) interp1(lt, R, x, 'pchip', 1000);
-% find interpolation points for inversion
-yint = zeros(1, np); % accumulator
-g = 7000; % initial guess
-for n = 0:np
-	yint(n+1) = newton(@(x) F(x) - (n/np), g, 1e-3);
-	g = yint(n+1); % reset guess
-end
-% sample random points
-xi = rand(1, N);
-n = floor(np * xi);
-u = np * xi - n;
-n = n+1; % account for one based indexing in vector operations
-y = yint(n) + (yint(n+1) - yint(n)).*u;
-
-root = newton(@(x) F(x) - 1.5, 300, 1e-4)
-F(root)
+% generate y_n points
+nN = linspace(0, N, N) ./ N; % values to find inverses of
+yint = arrayfun(@(n) tablefind(lt, R, n), nN(1:end-1));
+% solve for random points
+xi = rand(1, nbundles);
+n = floor(yint.*N)
