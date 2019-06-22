afb14bebde1b0cefd4c868125b165fe1c9d9c98b
Benjamin DalFavero <bdalfavero@scu.edu>
2019-06-22 13:09:23 -0700

    fix bad distribution

--- code/black_body.m
+++ code/black_body.m
@@ -15,28 +15,10 @@ xi = rand(1, N);
 lT_num = arrayfun(@(x) tablefind(lt, F, x), xi);
 lambda_num = lT_num / T;
 
-% test that root finder works
-num = interp1(lt, F, lT_num); % cdf values from numerical inversion
-err = abs(num - xi); % average error in the inversion, should be small
-mean_err = mean(err)
-max_err = max(err)
-
-%% sort wavelengths into historgram bins
-nbins = 15;
-% lambda values from table
-lambda = lt / T;
-% bins for histgram
-lambda_hist = linspace(min(lambda), max(lambda), nbins);
-% bundles for each bin
-count_hist = histcounts(lambda_num, nbins);
-
-%% get energy per bundle and get energy for each bin
+%% get energy per bundle
 sigma = 5.670374e-8; % stefan-boltzman constant
 e = sigma * T^4 / N;
-energy_bins = e * count_hist;
 
-%% plot results
-plot(lambda_hist, energy_bins, '--');
-xlabel('\lambda (\mu m)')
-ylabel('E (ergs)')
-title('Energy emitted vs wavelength')
+%% histrogram of wavelength distribution
+nbins = 15;
+histogram(lambda_num, nbins)
