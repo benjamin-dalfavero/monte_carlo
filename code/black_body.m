0ded7a4d34a77e3ea13403815fa70b937b950713
Benjamin DalFavero <bdalfavero@scu.edu>
2019-06-25 15:29:38 -0700

    add exact result

--- code/black_body.m
+++ code/black_body.m
@@ -1,26 +1,34 @@
 %% black body radiation
 
 %% get data from table
-dat = readmatrix('../data/black_body.csv');
+dat = csvread('../data/black_body.csv');
 lt = dat(:, 1); % lambda * T
 F = dat(:, 2); % associated CDF value
+IsT = dat(:, 3); % Intensity / sigma T^5
+I_norm = dat(:, 4); % normalized intensity
 
 %% get temperature and number of bins
-N = input('number of bins: ');
+N = input('number of bundles: ');
 T = input('temperature in K: ');
+nbins = input('number of bins in distribution: ');
 
 %% numerically invert cdf
 xi = rand(1, N);
 % find lambda * T for each xi and convert to lambda
 lT_num = arrayfun(@(x) tablefind(lt, F, x), xi);
-% take only lT where concentrated
+% take lT only where concentrated
 lT_conc = lT_num(and((lT_num >= 1.5e4), (lT_num <= 4e4)));
+% get wavelengths
 lambda_num = lT_conc / T;
 
-%% get energy per bundle
-sigma = 5.670374e-8; % stefan-boltzman constant
+%% energy per bin
+sigma = 5.670374e-8; % W m^-2 K^-4
 e = sigma * T^4 / N;
 
-%% histrogram of wavelength distribution
-nbins = 200;
-histogram(lambda_num, nbins)
+%% sort into bins and give results
+[bin_counts, lambda_bins] = histcounts(lambda_num, nbins);
+% get energy per bin
+energy_bins = e * bin_counts;
+results = [lambda_bins; bin_counts; energy_bins];
+filename = [num2str(T), 'K', num2str(N), 'bins.csv'];
+csvwrite(filename, results)
