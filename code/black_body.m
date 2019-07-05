dcf310f755d2f2dc64ccb717852125e7a4e73699
Benjamin DalFavero <bdalfavero@scu.edu>
2019-07-04 21:01:26 -0700

    removed filename error

--- code/black_body.m
+++ code/black_body.m
@@ -12,6 +12,7 @@ sigma = 5.670374419e-8; % W m^-2 K^-4
 N = input('number of bundles: ');
 T = input('temperature in K: ');
 nbins = input('number of bins in distribution: ');
+filename = input('name of output file: ', 's');
 
 %% numerically invert cdf
 xi = rand(1, N);
@@ -19,13 +20,13 @@ xi = rand(1, N);
 lT_num = arrayfun(@(x) tablefind(lt, F, x), xi);
 lambda_num = lT_num / T;
 
-%% sort wavelengths into bins 
+%% sort wavelengths into bins
 % define bins
-% make nbins-1 number of linearly spaced bins in concentrated region
+% make nbins number of linearly spaced bins in concentrated region
 % planck function is concentrated from 0 < lT < 4e4
 lambda_min = 0;
 lambda_max = 4e4 / T;
-edges_conc = linspace(lambda_min, lambda_max, (nbins-1));
+edges_conc = linspace(lambda_min, lambda_max, nbins);
 % add last point to capture wavelengths outside the concentrated region
 edges = [edges_conc, max(lt) / T];
 % sort into bins
@@ -41,18 +42,17 @@ energy = ebundle * counts;
 % get midpoint wavelength of each bin
 lam_mid = diff(edges);
 lam_mid = [lam_mid, edges(end) - edges(end - 1)];
-% dividge energy by wavelengths to get emissive power
+% divide energy by wavelengths to get emissive power
 power = energy ./ lam_mid;
-% exact solution for emissive power 
-IsT_exact = interp1(lt, IsT, lam_mid * T);
+% interpolate exact solution for emissive power
+IsT_exact = interp1(lt, IsT, edges * T);
 I_exact = IsT_exact * sigma * T^5;
 Eb_exact = I_exact * pi;
-% error in approximation
-err = abs(power - Eb_exact);
+% percent error in approximation
+err = abs(power - Eb_exact) ./ abs(Eb_exact);
 
 %% write results to a table
 res = [edges; counts; energy; power; Eb_exact; err]';
-headings = {'wavelength', 'energy', 'power', 'Elb', 'error'};
+headings = {'wavelength', 'bundles', 'energy', 'power', 'Elb', 'error'};
 res_table = array2table(res, 'VariableNames', headings);
-filename = 'results.csv';
 writetable(res_table, filename)
