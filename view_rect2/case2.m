e2c8e458d2565313313ebfd42e71cdbf79ee53e4
Benjamin DalFavero <bdalfavero@scu.edu>
2019-07-20 20:50:06 -0700

    first commit for this directory

---
+++ view_rect2/case2.m
@@ -0,0 +1,42 @@
+function case2(fname)
+% compute numerical view factor for second case
+
+% read parameters from file
+param = readtable(fname);
+Dx = param.Dx;
+Dy = param.Dy;
+Dz = param.Dz;
+x = param.x;
+y = param.y;
+N = param.N;
+% accumulators
+F = zeros(1, N);
+ncount = 0; % bundles that have hit
+% sample for each bundle
+for i = 1:N
+    % get angles from random nubmers
+    R_theta = rand();
+    R_phi = rand();
+    theta = asin(sqrt(R_theta));
+    phi = 2 * pi * R_phi;
+    % get y and z componenets of displacement vector
+    y2 = (Dx - x) * tan(phi);
+    z2 = (Dx - x) * cot(theta) * sec(phi);
+    % get final positions
+    yf = y + y2;
+    zf = z2;
+    % determine if collisison happens
+    if (yf >= 0) && (yf <= Dy) && (zf >= 0) && (zf <= Dz)
+        ncount = ncount + 1;
+    end
+    % store current view factor
+    F(i) = ncount / i;
+end
+% write to table
+headings = {'n', 'F'};
+res = [(1:N); F]';
+results = array2table(res, 'VariableNames', headings);
+fname_out = strrep(fname, '.csv', '_out.csv');
+fname_out = strrep(fname_out, 'param', 'results');
+writetable(results, fname_out);
+end
---
+++ view_rect2/runcase.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# runcase.py - run matlab scripts and plot results
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+
+# get name of input file from command line arguments
+fname = sys.argv[1]
+# quote to be compatible with bash
+quoted_fname = "'\"" + fname + "\"'"
+
+# run monte carlo program and get data from output file
+os.system("matrun case2 %s" % quoted_fname)
+fname_res = fname.replace('.csv','_out.csv').replace('param','results')
+data = pd.read_csv(fname_res)
+F = data.F
+n = data.n
+
+# run exact solution program and get value
+os.system("matrun exact %s" % quoted_fname)
+fname_exact = fname.replace('.csv','_exact.csv').replace('param','exact')
+file = open(fname_exact, 'r')
+F_ex = float(file.read())
+
+# read data from parameter file to generate plot title
+pdat = pd.read_csv(fname)
+ttl = r"$A_2$ %0.1f x %0.1f x %0.1f, $dA_1$ at (%0.1f, %0.1f)" \
+    % (pdat.Dx, pdat.Dy, pdat.Dz, pdat.x, pdat.y)
+
+# graph results and save in directory
+# plot data from results file
+plt.plot(n, F)
+# plot dashed line for exact value
+plt.plot([0, max(n)], [F_ex, F_ex], '--')
+# labels
+plt.xlabel('Bundles')
+plt.ylabel(r'$F_{12}$')
+plt.title(ttl)
+# generate filename and save
+fname_plot = fname.replace('.csv','.jpg').replace('param','plots')
+plt.savefig(fname_plot)
